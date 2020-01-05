92c8988e3aef94535e9f560e93f31605c3d792d7
张梓径 <zzj0402@gmail.com>
2020-01-05 16:17:35 +1300

    Create prepare-data.sh

---
+++ prepare-data.sh
@@ -0,0 +1,7 @@
+python -m language.question_answering.bert_joint.prepare_nq_data \
+  --logtostderr \
+  --input_jsonl /mnt/data/v1.0/nq-train-??.jsonl.gz \
+  --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
+  --max_seq_length=512 \
+  --include_unknowns=0.02 \
+  --vocab_file=bert-joint-baseline/vocab-nq.txt
