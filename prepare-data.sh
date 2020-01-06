8d12d670b1ffbc7b88ed0ed4cb17fcf32b86b15b
张梓径 <zzj0402@gmail.com>
2020-01-06 20:09:47 +1300

    Update prepare-data.sh

--- prepare-data.sh
+++ prepare-data.sh
@@ -1,6 +1,6 @@
 python -m language.question_answering.bert_joint.prepare_nq_data \
   --logtostderr \
-  --input_jsonl /root/data/v1.0/nq-train-??.jsonl.gz \
+  --input_jsonl /root/data/v1.0/train/nq-train-??.jsonl.gz \
   --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
   --max_seq_length=512 \
   --include_unknowns=0.02 \
