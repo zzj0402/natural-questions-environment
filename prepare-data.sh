210b4db32d16876cfdf968ee3aef5163067c0183
张梓径 <zzj0402@gmail.com>
2020-01-08 09:13:41 +1300

    Update prepare-data.sh
    output to different records.

--- prepare-data.sh
+++ prepare-data.sh
@@ -7,7 +7,7 @@ do
   python -m language.question_answering.bert_joint.prepare_nq_data \
   --logtostderr \
   --input_jsonl /root/data/v1.0/train/nq-train-0$counter.jsonl.gz \
-  --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
+  --output_tfrecord ~/output_dir/nq-train.tfrecords-0000$counter-of-00050 \
   --max_seq_length=512 \
   --include_unknowns=0.02 \
   --vocab_file=bert-joint-baseline/vocab-nq.txt
@@ -20,7 +20,7 @@ do
   python -m language.question_answering.bert_joint.prepare_nq_data \
   --logtostderr \
   --input_jsonl /root/data/v1.0/train/nq-train-$counter.jsonl.gz \
-  --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
+  --output_tfrecord ~/output_dir/nq-train.tfrecords-000$counter-of-00050 \
   --max_seq_length=512 \
   --include_unknowns=0.02 \
   --vocab_file=bert-joint-baseline/vocab-nq.txt
