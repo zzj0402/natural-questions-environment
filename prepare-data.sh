a41086cdd7b65d1d9f021de99208059e426feba7
张梓径 <zzj0402@gmail.com>
2020-01-07 17:10:45 +1300

    Update prepare-data.sh
    all shards processing

--- prepare-data.sh
+++ prepare-data.sh
@@ -1,7 +1,15 @@
-python -m language.question_answering.bert_joint.prepare_nq_data \
+#!/bin/bash
+counter=00
+while [ $counter -le 49 ]
+do
+  echo Processing training shard $counter
+  python -m language.question_answering.bert_joint.prepare_nq_data \
   --logtostderr \
-  --input_jsonl /root/data/v1.0/train/nq-train-??.jsonl.gz \
+  --input_jsonl /root/data/v1.0/train/nq-train-$counter.jsonl.gz \
   --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
   --max_seq_length=512 \
   --include_unknowns=0.02 \
   --vocab_file=bert-joint-baseline/vocab-nq.txt
+done
+
+echo All $counter  training data shards processed!
