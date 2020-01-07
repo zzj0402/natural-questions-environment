43b92c0e6d001edf2ab76f88a7eb987a89deed7f
张梓径 <zzj0402@gmail.com>
2020-01-07 18:23:42 +1300

    Update prepare-data.sh
    counter fix: the first 00 increments to 1 instead of 01

--- prepare-data.sh
+++ prepare-data.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-counter=00
+counter=0
+
+while [ $counter -le 9 ]
+do
+  echo Processing training shard $counter ...
+  python -m language.question_answering.bert_joint.prepare_nq_data \
+  --logtostderr \
+  --input_jsonl /root/data/v1.0/train/nq-train-0$counter.jsonl.gz \
+  --output_tfrecord ~/output_dir/nq-train.tfrecords-00000-of-00001 \
+  --max_seq_length=512 \
+  --include_unknowns=0.02 \
+  --vocab_file=bert-joint-baseline/vocab-nq.txt
+  ((counter++))
+done
+
 while [ $counter -le 49 ]
 do
   echo Processing training shard $counter ...
